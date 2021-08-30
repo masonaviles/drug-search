7efa0f7e23c5b22f7d7d831157a956239b0aef58
Mason Aviles <mce.aviles@gmail.com>
2021-08-29 20:49:42 -0700

    add working map of json data

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
-// const [data, setData] = useState([]);
-//   const [query, setQuery] = useState('cats');
-//   const [isLoading, setIsLoading] = useState(true);
-
-//   const performSearch = (value) => setQuery(value);
-
-//   useEffect(() => { 
-//     // Be sure to add your API key
-//     axios(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=rXDJp5KfBOkYxmn2kxAUQSS2SJNt7PlB`)
-//       .then(response => setData(response.data.data))
-//       .catch(error => console.log('Error fetching and parsing data', error))
-//       .finally(() => setIsLoading(false));
-//   }, [query]);
-
 function App() {
-  // const [ interaction, setInteraction ] = useState(null);
-  // const [ interactionSeverity, setInteractionSeverity ] =  useState([]);
+  const [posts, setPosts] = useState([])
 
   useEffect(() => {
 		fetch("https://jsonplaceholder.typicode.com/posts")
 			.then((response) => response.json())
-			.then((data) => console.log(data))
+			.then((data) => {
+				setPosts(data) // new
+			})
 	}, [])
 
   return (
     <div className="App">
-
+        <h1>Drug Search</h1>
+        <div>
+        {posts.map((item) => (
+          <li>
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+          </li>
+        ))}
+        </div>
     </div>
   );
 }
