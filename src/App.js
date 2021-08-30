b644afab4287a7798ff0c215e54efd8098b54c02
Mason Aviles <mce.aviles@gmail.com>
2021-08-29 23:13:02 -0700

    add a good state, searchfield changing state, just need to take state to filter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import SearchForm from './SearchForm';
 
 
 function App() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState('finasteride');
 
   useEffect(() => {
 		fetch('/data.json')
@@ -14,23 +15,30 @@ function App() {
 	}, []);
 
   console.log('data from state: ', data);
+  console.log('query from state: ', query);
+  
+  let testSearch = data.filter(word => word.drugs.includes('finasteride'));
 
-  const testSearch = data.filter(word => word.drugs.includes('finasteride'));
+  const performSearch = (value) => setQuery(value);
 
-  //console.log('sample search', testSearch[0].drugs[0]);
+  console.log('testSearch: ', testSearch);
+  console.log('performSearch: ', performSearch);
 
   return (
     <div className="App">
         <h1>Drug Search</h1>
         <div>
-          <SearchForm/>
+          <SearchForm onSearch={performSearch}/>
         </div>
         <ul>
         {testSearch.map((interactions) => (
-          <li>
-            <h2>{interactions.drugs[0]}</h2>
-            <h3>{interactions.drugs[1]}</h3>
-            <p>{interactions.severity}</p>
+          <li key={Math.floor(Math.random()*1000000)}>
+            <h2>
+              <span>{interactions.drugs[0]}</span>
+              <span>x</span>
+              <span>{interactions.drugs[1]}</span>
+            </h2>
+            <h3>{interactions.severity}</h3>
             <p>{interactions.description}</p>
           </li>
         ))}
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 function SearchForm(props) {
+  
   const [searchText, setSearchText] = useState('');
 
   const onSearchChange = (e) => setSearchText(e.target.value);
@@ -11,6 +12,8 @@ function SearchForm(props) {
     e.currentTarget.reset();
   }
 
+  console.log("search text: ", searchText);
+
   return (
     <form className="search-form" onSubmit={handleSubmit}>
       {/* <label className="is-hidden" htmlFor="search">Search</label> */}
@@ -19,7 +22,7 @@ function SearchForm(props) {
         name="search"
         placeholder="Search..."
       />
-      <button type="submit" id="submit" className="search-button"><i className="material-icons icn-search">search</i></button>
+      <button type="submit" id="submit" className="search-button">search</button>
     </form>
   );
 }
